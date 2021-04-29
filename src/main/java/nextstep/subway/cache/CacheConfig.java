28c34327487fdbd2ad32392fbfa32ed85f614d93
wyh951753 <wyh951753@gmail.com>
2021-04-30 01:37:46 +0900

    fix: Redis 캐시로 인한 빌드 실패 이슈 해결

--- src/main/java/nextstep/subway/cache/CacheConfig.java
+++ src/main/java/nextstep/subway/cache/CacheConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,6 +17,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 @RequiredArgsConstructor
 @Configuration
+@Profile("prod")
 public class CacheConfig extends CachingConfigurerSupport {
     private final RedisConnectionFactory connectionFactory;
 
